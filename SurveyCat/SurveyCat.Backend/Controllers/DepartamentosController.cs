eb03c9709b722fd27b8f7b34c7f17ec53c58ea25
AntonioCD <62318646+AntonioCD@users.noreply.github.com>
2026-03-20 16:40:23 -0600

    Creando controladores genéricos

---
+++ SurveyCat/SurveyCat.Backend/Controllers/DepartamentosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurveyCat.Backend.Data;
+using SurveyCat.Shared.Entities;
+using System.Threading.Tasks;
+
+namespace SurveyCat.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartamentosController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public DepartamentosController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Departamento departamento)
+        {
+            _context.Departamentos.Add(departamento);
+            await _context.SaveChangesAsync();
+            return Ok(departamento);
+        }
+    }
+}
--- SurveyCat/SurveyCat.Shared/Entities/Departamento.cs
+++ SurveyCat/SurveyCat.Shared/Entities/Departamento.cs
@@ -5,22 +5,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SurveyCat.Shared.Entities
+namespace SurveyCat.Shared.Entities;
+
+public class Departamento
 {
-    public class Departamento
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        [Display(Name = "Cod. Dpto")]
-        [MaxLength(2, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        public string CodDepto { get; set; } = null!;
+    [Display(Name = "Cod. Dpto")]
+    [MaxLength(2, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string CodDepto { get; set; } = null!;
 
-        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        public string Nombre { get; set; } = null!;
+    [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string Nombre { get; set; } = null!;
 
-        [MaxLength(2, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
-        public string? CodINIDE { get; set; }
-    }
+    [MaxLength(2, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+    public string? CodINIDE { get; set; }
 }
